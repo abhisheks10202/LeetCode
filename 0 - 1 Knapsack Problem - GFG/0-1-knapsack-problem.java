e8e1695d76b07d6e09ec3113edd46a305956dbe4
Abhishek Singh <78976779+abhisheks10202@users.noreply.github.com>
2022-06-28 09:09:36 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])throws IOException
+    {
+        //reading input using BufferedReader class
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //reading total testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            //reading number of elements and weight
+            int n = Integer.parseInt(read.readLine());
+            int w = Integer.parseInt(read.readLine());
+            
+            int val[] = new int[n];
+            int wt[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            //inserting the values
+            for(int i = 0; i < n; i++)
+              val[i] = Integer.parseInt(st[i]);
+             
+            String s[] = read.readLine().trim().split("\\s+"); 
+            
+            //inserting the weigths
+            for(int i = 0; i < n; i++)
+              wt[i] = Integer.parseInt(s[i]);
+              
+            //calling method knapSack() of class Knapsack
+            System.out.println(new Solution().knapSack(w, wt, val, n));
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution 
+{ 
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+         // your code here 
+          int t[][]=new int[n+1][W+1];
+    
+            for(int i=0;i<t[0].length;i++)
+                t[0][i]=0;
+            for(int i=0;i<t.length;i++)
+                 t[i][0]=0;
+        for(int i=1;i<t.length;i++)
+        {
+            for(int j=1;j<t[0].length;j++)
+            {
+                if(wt[i-1]<=j){
+                    t[i][j]=Math.max(val[i-1]+t[i-1][j-wt[i-1]],t[i-1][j]);
+                }
+                else
+                t[i][j]=t[i-1][j];
+            }
+        }
+        return t[n][W];
+        
+    } 
+}
+
+
