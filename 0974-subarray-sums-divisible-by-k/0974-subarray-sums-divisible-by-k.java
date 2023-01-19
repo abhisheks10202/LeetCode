82d4eadeee02a5080b58b53ef3520a4819cd88aa
Abhishek Singh <78976779+abhisheks10202@users.noreply.github.com>
2023-01-19 21:43:47 +0530

    Time: 19 ms (74.28%), Space: 48 MB (47.65%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,23 +1,33 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        Map<Integer,Integer>hm=new HashMap<>();
-        hm.put(0,1);
-        int sum=0;
-        int rem=0;
-        int result=0;
+//         Map<Integer,Integer>hm=new HashMap<>();
+//         hm.put(0,1);
+//         int sum=0;
+//         int rem=0;
+//         int result=0;
         
-        for(int val:nums)
-        {
-            sum+=val;
-            rem=sum%k;
-            if(rem<0)rem=rem+k;
-            if(hm.containsKey(rem))
-            {
-                result+=hm.get(rem);
-                hm.put(rem,hm.get(rem)+1);
-            }
-            else hm.put(rem,1);
+//         for(int val:nums)
+//         {
+//             sum+=val;
+//             rem=sum%k;
+//             if(rem<0)rem=rem+k;
+//             if(hm.containsKey(rem))
+//             {
+//                 result+=hm.get(rem);
+//                 hm.put(rem,hm.get(rem)+1);
+//             }
+//             else hm.put(rem,1);
+//         }
+//         return result;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0, remainder; i < nums.length; i++) {
+            if (i > 0) nums[i] += nums[i - 1];
+            remainder = (nums[i] % k + k) % k;
+            map.put(remainder, map.getOrDefault(remainder, 0) + 1);
         }
+        int result = map.getOrDefault(0, 0);
+        for (int frequency : map.values())
+            result += frequency * (frequency - 1) / 2;
         return result;
     }
 }
