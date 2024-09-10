3d7d91d1dced41beb30da4747f1f0e9256062405
Abhishek Singh <78976779+abhisheks10202@users.noreply.github.com>
2024-09-10 14:00:18 +0530

    Time: 12 ms (34.28%) | Memory: 44.8 MB (77.50%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
@@ -11,7 +11,7 @@
 class Solution {
     public int gcd(int a,int b)
     {
-            if (a == 0)
+           if (a == 0)
             return b;
         if (b == 0)
             return a;
@@ -21,8 +21,13 @@ class Solution {
             return a;
 
         // a is greater
-        if (a > b)
+        if (a > b) {
+            if (a % b == 0)
+                return b;
             return gcd(a - b, b);
+        }
+        if (b % a == 0)
+            return a;
         return gcd(a, b - a);
     }
     public ListNode insertGreatestCommonDivisors(ListNode head) {
